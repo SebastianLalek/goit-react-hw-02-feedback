93ed713b441602b9bcc8b0ae4ee34b0496bf9d5a
Sebastian Lalek <chemiczq@gmail.com>
2023-10-24 10:19:46 +0200

    add countTotalFeedback method

--- src/components/feedback/Feedback.js
+++ src/components/feedback/Feedback.js
@@ -15,17 +15,30 @@ export default class Feedback extends Component {
     }));
   };
 
+  countTotalFeedback = () => {
+    let sum;
+    this.setState(state => {
+      sum = state.good + state.neutral + state.bad;
+      return { total: sum };
+    });
+  };
+
+  handleClick = e => {
+    this.counter(e);
+    this.countTotalFeedback();
+  };
+
   render() {
     return (
       <div>
         <h2>Please leave feedback</h2>
-        <button className={css.button} id="good" onClick={this.counter}>
+        <button className={css.button} id="good" onClick={this.handleClick}>
           Good
         </button>
-        <button className={css.button} id="neutral" onClick={this.counter}>
+        <button className={css.button} id="neutral" onClick={this.handleClick}>
           Neutral
         </button>
-        <button className={css.button} id="bad" onClick={this.counter}>
+        <button className={css.button} id="bad" onClick={this.handleClick}>
           Bad
         </button>
         <h2>Statistics</h2>
@@ -33,6 +46,7 @@ export default class Feedback extends Component {
           <li>Good:{this.state.good}</li>
           <li>Neutral:{this.state.neutral}</li>
           <li>Bad:{this.state.bad}</li>
+          <li>Total:{this.state.total}</li>
         </ul>
       </div>
     );
