4ac653d6e37fd888ebed55ca7a0d41d8f2aed3a6
Sebastian Lalek <chemiczq@gmail.com>
2023-10-24 11:57:50 +0200

    export Statistics method

---
+++ src/components/statistics/Statistics.js
@@ -0,0 +1,11 @@
+function Statistics({ good, neutral, bad, total, positivePercentage }) {
+  return (
+    <ul>
+      <li>{good}</li>
+      <li>{neutral}</li>
+      <li>{bad}</li>
+      <li>{total}</li>
+      <li>{positivePercentage}</li>
+    </ul>
+  );
+}
